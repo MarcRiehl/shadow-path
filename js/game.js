8fd0f1f1f672688d05edb05ba4b94d5118eb071b
MarcRiehl <210008956+MarcRiehl@users.noreply.github.com>
2026-04-20 15:59:37 +0200

    feat: class MovableObject

--- js/game.js
+++ js/game.js
@@ -1,3 +1,11 @@
+let canvas;
+let ctx;
+let charcter = new Image();
+
 function init() {
-    
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext("2d");
+    charcter.src = '../img/2_character_pepe/2_walk/W-21.png';
+
+    ctx.drawImage(charcter, 20, 20, 50, 150);
 }
