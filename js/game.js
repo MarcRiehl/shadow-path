6ebb5ff9b1a0176206587b9a47835fcae71bd90a
MarcRiehl <210008956+MarcRiehl@users.noreply.github.com>
2026-04-20 18:15:37 +0200

    feat: world draw()

--- js/game.js
+++ js/game.js
@@ -1,15 +1,11 @@
 let canvas;
 let ctx;
-let charcter = new Character();
-let enemies = [
-new Chicken(),
-new Chicken(),
-new Chicken()
-];
+let world;
 
 function init() {
+
     canvas = document.getElementById('canvas');
-    ctx = canvas.getContext("2d");
-    console.log('My charcter is', charcter);
+    world = new World(canvas);
+    console.log('My charcter is', world.character);
     
 }
--- models/world.class.js
+++ models/world.class.js
@@ -1,3 +1,39 @@
 class World {
-    
+    character = new Character();
+    enemies = [
+        new Chicken(),
+        new Chicken(),
+        new Chicken()
+    ];
+    canvas;
+    ctx;
+
+    constructor(canvas) {
+        this.ctx = canvas.getContext("2d");
+        this.canvas = canvas;
+        this.draw();
+    }
+
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+        // for (let index = 0; index < this.enemies.length; index++) {
+        //  this.ctx.drawImage(this.enemies[index].img, this.enemies[index].x, this.enemies[index].y, this.enemies[index].width, this.enemies[index].height);
+
+        // }
+        this.enemies.forEach(enemy => {
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+        });
+
+
+
+        //Draw() wird immer wieder aufgerufen
+        let self = this; //wichtig!!!! kennt sonst in der Funktion darunter this nicht mehr
+        requestAnimationFrame(function () {
+            self.draw();
+        });
+
+
+    }
+
 }
