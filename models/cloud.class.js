740f0e3c03cba9817b6785e4361f6f106d65b7ef
MarcRiehl <210008956+MarcRiehl@users.noreply.github.com>
2026-04-21 13:48:10 +0200

    feat: animate(), chicken, character

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -6,9 +6,13 @@ class Cloud extends MovableObject {
     constructor() {
         super().loadImage('../img/5_background/layers/4_clouds/1.png');
         this.x = Math.random() * 500; // Zahl zwische 200 und 700 x
-        this.y = 20;
-        this.width = 500;
-        this.height = 300;
+        this.animate();
+
     }
+
+    animate() {
+        this.moveLeft();
+    }
+
 
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,14 +1,26 @@
 class MovableObject {
     x = 120;
-    y = 290;
+    y = 280;
     img;
     height = 150;
     width = 100;
+    imageCache = [];
+    currentImage = 0;
+    speed = 0.15;
 
     //loadImage('img/test.png')
     loadImage(path) {
         this.img = new Image(); // this.img = document.getElementById('image') <img id="image" src>
         this.img.src = path;
+    }
+
+    // arr = 'img1.png, 'img2.png' usw.
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
     }
 
     moveRight() {
@@ -17,7 +29,9 @@ class MovableObject {
     }
 
     moveLeft() {
-
+        setInterval(() => {
+            this.x -= this.speed;
+        }, 1000 / 60);
     }
 
 }
