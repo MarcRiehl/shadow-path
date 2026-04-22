06ec450e9eda0dd8aa64b9cce79caef24adc2ed1
MarcRiehl <210008956+MarcRiehl@users.noreply.github.com>
2026-04-22 20:16:24 +0200

    fix: path img

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -3,14 +3,14 @@ class Chicken extends MovableObject {
     height = 75;
     width = 100;
     IMAGES_WALKING = [
-        '../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
-        '../img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
-        '../img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+        './img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+        './img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+        './img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
     ];
 
 
     constructor() {
-        super().loadImage('../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+        super().loadImage('./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
 
 
--- models/cloud.class.js
+++ models/cloud.class.js
@@ -4,7 +4,7 @@ class Cloud extends MovableObject {
     height = 300;
 
     constructor() {
-        super().loadImage('../img/5_background/layers/4_clouds/1.png');
+        super().loadImage('./img/5_background/layers/4_clouds/1.png');
         this.x = Math.random() * 500; // Zahl zwische 200 und 700 x
         this.animate();
 
