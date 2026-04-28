30cc33519a6450aa5a19359d4a9b313bfcec37d7
MarcRiehl <210008956+MarcRiehl@users.noreply.github.com>
2026-04-28 14:16:26 +0200

    feat: endboss ()

--- levels/level1.js
+++ levels/level1.js
@@ -34,7 +34,10 @@ level1 = new Level(
         new BackgroundObject('./img/5_background_shadow_path/armory_street/layer_02.png', 720 * 3),
         new BackgroundObject('./img/5_background_shadow_path/armory_street/layer_00.png', 720 * 4),
         new BackgroundObject('./img/5_background_shadow_path/armory_street/layer_01.png', 720 * 4),
-        new BackgroundObject('./img/5_background_shadow_path/armory_street/layer_02.png', 720 * 4)
+        new BackgroundObject('./img/5_background_shadow_path/armory_street/layer_02.png', 720 * 4),
+        new BackgroundObject('./img/5_background_shadow_path/armory_street/layer_00.png', 720 * 5),
+        new BackgroundObject('./img/5_background_shadow_path/armory_street/layer_01.png', 720 * 5),
+        new BackgroundObject('./img/5_background_shadow_path/armory_street/layer_02.png', 720 * 5)
     ]
 );
 
--- models/endboss.class.js
+++ models/endboss.class.js
@@ -5,9 +5,9 @@ class Endboss extends MovableObject {
         y = -55;
         offset = {
                 top: 180,
-                bottom: 60 ,
+                bottom: 60,
                 left: 200,
-                right: 140 
+                right: 140
         }
 
         IMAGES_WALKING = [
@@ -36,12 +36,25 @@ class Endboss extends MovableObject {
                 './img/4_enemie_boss_troll/jump/troll_02_1_jump_009.png'
 
         ];
+        IMAGES_IDLE = [
+                './img/4_enemie_boss_troll/idle/troll_02_1_idle_000.png',
+                './img/4_enemie_boss_troll/idle/troll_02_1_idle_001.png',
+                './img/4_enemie_boss_troll/idle/troll_02_1_idle_002.png',
+                './img/4_enemie_boss_troll/idle/troll_02_1_idle_003.png',
+                './img/4_enemie_boss_troll/idle/troll_02_1_idle_004.png',
+                './img/4_enemie_boss_troll/idle/troll_02_1_idle_005.png',
+                './img/4_enemie_boss_troll/idle/troll_02_1_idle_006.png',
+                './img/4_enemie_boss_troll/idle/troll_02_1_idle_007.png',
+                './img/4_enemie_boss_troll/idle/troll_02_1_idle_008.png',
+                './img/4_enemie_boss_troll/idle/troll_02_1_idle_009.png'
+        ];
 
         constructor() {
                 super().loadImage(this.IMAGES_WALKING[0]); // erstes Standbild laden Array[0]
                 this.loadImages(this.IMAGES_WALKING);
                 this.loadImages(this.IMAGES_JUMP);
-                this.x = 2250;
+                this.loadImages(this.IMAGES_IDLE);
+                this.x = 3250;
                 this.otherDirection = true;
                 this.animate();
 
@@ -52,13 +65,26 @@ class Endboss extends MovableObject {
                 setInterval(() => {
                         if (i < 10 && this.firstContact) {
                                 this.playAnimation(this.IMAGES_JUMP);
-                        } else {
+                                this.x = world.character.x - 20;
+                                this.speed = 0.5;
+                        } else if (this.firstContact) {
+                                this.playAnimation(this.IMAGES_WALKING);
+                                this.x = world.character.x - Math.random() * 100;
+                                this.speed = 0.15 + Math.random() * 0.1;
+                        }
+                        else {
                                 this.playAnimation(this.IMAGES_WALKING);
                                 // this.x = world.character.x;
-
+                                this.x = 3200 + Math.random() * 50;
+                                this.speed = 0.15 + Math.random() * 0.1;
                         }
                         i++;
-                        if (world.character.x > 2000 && !this.firstContact) {
+                        if (world.character.isDead()) {
+                                 this.playAnimation(this.IMAGES_IDLE);
+                                this.speed = 0;
+                                this.x = world.character.x + 120;
+                        }
+                        if (world.character.x > 3000 && !this.firstContact) {
                                 i = 0;
                                 this.firstContact = true;
 
